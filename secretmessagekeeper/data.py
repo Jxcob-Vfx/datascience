07dc8bd55019160b24173fd7c84b81e3f66063f0
Jxcob-Vfx <109388149+Jxcob-Vfx@users.noreply.github.com>
2022-11-28 21:35:46 -0800

    Create data.py

---
+++ secretmessagekeeper/data.py
@@ -0,0 +1,6 @@
+# data file for secretmessagekeeper.py; must be in the same system folder during runtime
+# holds dictionary of usernames and secure hashed passwords
+
+# default dictionary with value data
+
+data = {}
